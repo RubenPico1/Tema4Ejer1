e9b512b0c41f4c8d68b5c355cec0ac13f90b1713
picperru <picperru@lab03-21.gregoriofer.local>
2023-01-10 12:40:38 +0100

    Segunda confirmacion

--- Documents/NetBeansProjects/JavaAplicationstRP/src/javaaplicationstrp/Persona.java
+++ Documents/NetBeansProjects/JavaAplicationstRP/src/javaaplicationstrp/Persona.java
@@ -10,4 +10,36 @@ package javaaplicationstrp;
  */
 public class Persona {
     
+    String nombre;
+    String Apellido;
+    String Correo;   
+    
+    public Persona(){
+            
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }    
+        
 }
